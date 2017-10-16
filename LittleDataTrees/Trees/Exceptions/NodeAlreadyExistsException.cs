bea2f1e270cc2ea8ab0f7df2affb6d8b44549a22
little-apps <nick@little-apps.com>
2017-10-16 15:54:36 -0600

    Exceptions added for when node already exists or doesn't exist

---
+++ LittleDataTrees/Trees/Exceptions/NodeAlreadyExistsException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Trees.Exceptions
+{
+    public class NodeAlreadyExistsException : Exception
+    {
+        public readonly object ExistingValue;
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Constructor for NodeAlreadyExistsException
+        /// </summary>
+        /// <param name="value">Value that already exists.</param>
+        public NodeAlreadyExistsException(object value) : base("Node already exists with value.")
+        {
+            ExistingValue = value;
+        }
+    }
+}
---
+++ LittleDataTrees/Trees/Exceptions/NodeNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Trees.Exceptions
+{
+    public class NodeNotFoundException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// Constructor for NodeNotFoundException
+        /// </summary>
+        public NodeNotFoundException() : base("Node could not be found.")
+        {
+            
+        }
+    }
+}
