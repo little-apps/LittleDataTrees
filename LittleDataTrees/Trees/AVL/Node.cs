d8e6988412aca44aaee3065b8d28556320a86562
little-apps <nick@little-apps.com>
2017-10-16 21:04:25 -0600

    Added AVL Tree

---
+++ LittleDataTrees/Trees/AVL/Node.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Trees.Abstract;
+
+namespace Trees.AVL
+{
+    public class Node<T> : BaseTreeNode<Node<T>, T> where T : IComparable<T>
+    {
+        public object Tag;
+
+        public int MaxDepth
+        {
+            get
+            {
+                var left = Left?.MaxDepth ?? 0;
+                var right = Right?.MaxDepth ?? 0;
+
+                return Math.Max(left, right) + 1;
+            }
+        }
+
+        public Node(T value) : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Checks if this node or any of its child nodes contains the value.
+        /// </summary>
+        /// <param name="value">Value to search for</param>
+        /// <returns>True if this node or a child of it contains <paramref name="value"/></returns>
+        public bool Contains(T value)
+        {
+            var comparison = value.CompareTo(Value);
+
+            if (comparison == 0)
+                return true;
+
+            var nextNode = comparison < 0 ? Left : Right;
+
+            return nextNode != null && nextNode.Contains(value);
+        }
+
+        /// <summary>
+        /// Finds node with value in this node or it's children
+        /// </summary>
+        /// <param name="value">Value to search for</param>
+        /// <returns>Node where value exists or null if it wasn't found.</returns>
+        public Node<T> Find(T value)
+        {
+            var currentNode = this;
+
+            while (true)
+            {
+                if (currentNode == null)
+                    return null;
+
+                var comparison = value.CompareTo(currentNode.Value);
+
+                if (comparison < 0)
+                    currentNode = currentNode.Left;
+                else if (comparison > 0)
+                    currentNode = currentNode.Right;
+                else
+                    return currentNode;
+            }
+        }
+
+        
+
+        public override int CompareTo(IBaseNode<T> other)
+        {
+            if (ReferenceEquals(other, null))
+                return -1;
+
+            if (ReferenceEquals(this, other))
+                return 0;
+
+            return Value.CompareTo(other.Value);
+        }
+    }
+}
---
+++ LittleDataTrees/Trees/AVL/Tree.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using Trees.Abstract;
+using Trees.Exceptions;
+
+namespace Trees.AVL
+{
+    public class Tree<T> : BaseTree<Node<T>, T> where T : IComparable<T>
+    {
+        public uint Count { get; protected set; }
+
+        public Tree()
+        {
+            Root = null;
+        }
+
+        public override Node<T> Add(T value, object tag = null)
+        {
+            var newNode = new Node<T>(value) { Tag = tag };
+
+            Root = Add(Root, newNode);
+
+            CalcHeights();
+
+            Count++;
+
+            return newNode;
+        }
+
+        private static Node<T> Add(Node<T> current, Node<T> nodeToAdd)
+        {
+            if (current == null)
+                // The left/right node is null, so we can add it there
+                return nodeToAdd;
+
+            if (nodeToAdd.CompareTo(current) < 0)
+            {
+                current.Left = Add(current.Left, nodeToAdd);
+                current = BalanceTree(current);
+            }
+            else if (nodeToAdd.CompareTo(current) > 0)
+            {
+                current.Right = Add(current.Right, nodeToAdd);
+                current = BalanceTree(current);
+            }
+            else if (nodeToAdd.CompareTo(current) == 0)
+            {
+                throw new NodeAlreadyExistsException(current.Value);
+            }
+                
+            return current;
+        }
+
+        private void CalcHeights()
+        {
+            CalcHeights(Root, 1);
+        }
+
+        private static void CalcHeights(Node<T> node, uint height)
+        {
+            node.Height = height;
+
+            if (node.Left != null)
+                CalcHeights(node.Left, height + 1);
+
+            if (node.Right != null)
+                CalcHeights(node.Right, height + 1);
+        }
+
+        private static Node<T> BalanceTree(Node<T> node)
+        {
+            var balance = DetermineBalance(node);
+
+            if (balance >= 2)
+                node = DetermineBalance(node.Left) > 0 ? RotateLeftLeft(node) : RotateLeftRight(node);
+            else if (balance <= -2)
+                node = DetermineBalance(node.Right) > 0 ? RotateRightLeft(node) : RotateRightRight(node);
+
+            return node;
+        }
+
+        private static int DetermineBalance(Node<T> current)
+        {
+            var l = current.Left?.MaxDepth ?? 0;
+            var r = current.Right?.MaxDepth ?? 0;
+
+            return l - r;
+        }
+
+        private static Node<T> RotateRightRight(Node<T> node)
+        {
+            var pivot = node.Right;
+            node.Right = pivot.Left;
+            pivot.Left = node;
+            return pivot;
+        }
+
+        private static Node<T> RotateLeftLeft(Node<T> node)
+        {
+            var pivot = node.Left;
+            node.Left = pivot.Right;
+            pivot.Right = node;
+            return pivot;
+        }
+
+        private static Node<T> RotateLeftRight(Node<T> node)
+        {
+            var pivot = node.Left;
+            node.Left = RotateRightRight(pivot);
+            return RotateLeftLeft(node);
+        }
+
+        private static Node<T> RotateRightLeft(Node<T> node)
+        {
+            var pivot = node.Right;
+            node.Right = RotateLeftLeft(pivot);
+            return RotateRightRight(node);
+        }
+
+        /// <inheritdoc />
+        public override bool Contains(T value)
+        {
+            return !IsEmpty && Root.Contains(value);
+        }
+
+        /// <inheritdoc />
+        /// <exception cref="NodeNotFoundException">Thrown if node wasn't found in tree.</exception>
+        public override Node<T> Find(T value)
+        {
+            return Root?.Find(value) ?? throw new NodeNotFoundException();
+        }
+
+        public override void Clear()
+        {
+            Root = null;
+            Count = 0;
+        }
+
+        /// <inheritdoc />
+        /// <exception cref="NodeNotFoundException">Thrown if node was not found in tree.</exception>
+        public override void Delete(T value)
+        {
+            Root = Delete(Root, value);
+
+            CalcHeights();
+
+            Count--;
+        }
+
+        private static Node<T> Delete(Node<T> current, T value)
+        {
+            if (current == null)
+                // Node not found
+                throw new NodeNotFoundException();
+
+            if (value.CompareTo(current.Value) < 0)
+            {
+                current.Left = Delete(current.Left, value);
+                if (DetermineBalance(current) == -2)
+                    current = DetermineBalance(current.Right) <= 0
+                        ? RotateRightRight(current)
+                        : RotateRightLeft(current);
+            }
+            else if (value.CompareTo(current.Value) > 0)
+            {
+                current.Right = Delete(current.Right, value);
+                if (DetermineBalance(current) == 2)
+                    current = DetermineBalance(current.Left) >= 0 ? RotateLeftLeft(current) : RotateLeftRight(current);
+            }
+            else
+            {
+                if (current.Right != null)
+                {
+                    var parent = current.Right;
+
+                    while (parent.Left != null)
+                        parent = parent.Left;
+
+                    current.Value = parent.Value;
+                    current.Right = Delete(current.Right, parent.Value);
+
+                    if (DetermineBalance(current) == 2)
+                        current = DetermineBalance(current.Left) >= 0
+                            ? RotateLeftLeft(current)
+                            : RotateLeftRight(current);
+                }
+                else
+                {
+                    return current.Left;
+                }
+            }
+
+            return current;
+        }
+        
+    }
+}
